3f828418223a3cfadc93673df8b9721acabc8d00
Wallace W <wallace_wolv@hotmail.com>
2026-03-09 22:59:13 -0300

    feat: (133) Module Introduction & Starting Project

---
+++ 08_refs_portals/01-starting-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Player from "./components/Player";
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
+}
+
+export default App;
