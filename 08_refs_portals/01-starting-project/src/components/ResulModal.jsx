1914dac7957ba17b8fecc1a583313cff1093bd1c
Wallace W <wallace_wolv@hotmail.com>
2026-03-14 10:16:09 -0300

    feat: (143) Forwarding Refs to Custom Components
    Esse padrão só pode ser usado com React 19 ou acima

--- 08_refs_portals/01-starting-project/src/components/ResulModal.jsx
+++ 08_refs_portals/01-starting-project/src/components/ResulModal.jsx
@@ -8,9 +8,9 @@
  * @param {ResultModalProps} props
  */
 
-export default function ResultModal({ result, targetTime }) {
+export default function ResultModal({ ref, result, targetTime }) {
   return (
-    <dialog className="result-modal">
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>the target time was <strong>{targetTime}</strong> seconds.</p>
       <p>You stopped the timer with <strong>X seconds left.</strong></p>
--- 08_refs_portals/01-starting-project/src/components/TimerChallenge.jsx
+++ 08_refs_portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -12,6 +12,7 @@ import ResultModal from "./ResulModal";
  */
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -19,6 +20,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -30,7 +32,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
 
       <section className="challenge">
         <h2>{title}</h2>
